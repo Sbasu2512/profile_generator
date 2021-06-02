6178006815c0f4d049a44d42b2d0192155765960
Sayantan Basu <sayantanbasu@live.in>
2021-06-01 21:06:15 -0400

    final commit

--- survey.js
+++ survey.js
@@ -4,7 +4,7 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-
+// rl.question(,(answer)=>{});
 rl.question('What is your name? Nicknames are also acceptable :)', (answer) => {
   console.log(`your name is: ${answer}`);
   rl.close();
